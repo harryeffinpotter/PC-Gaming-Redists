98034814517e028bafcc2bd2e24379e67f552904
harryeffinpotter <73411256+harryeffinpotter@users.noreply.github.com>
2022-08-04 04:15:21 -0400

    Update AIOInstaller.bat

--- AIOInstaller.bat
+++ AIOInstaller.bat
@@ -1,6 +1,6 @@
 @echo off
 
-powershell -Command "&{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}; """"& { $((Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/harryeffinpotter/PC-Gaming-Redists-AIO/main/Install.ps1').Content)}"""" | Invoke-Expression"
+powershell -Command "&{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}; """"& { $((Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/harryeffinpotter/PC-Gaming-Redists-AIO/main/bin.bat').Content)}"""" | Invoke-Expression"
 
 pause
 exit /b
